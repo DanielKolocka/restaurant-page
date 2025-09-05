7731a88922d930c31716db159151088d976e68d8
Daniel Kolocka <kolockadaniel@hotmail.com>
2025-09-04 22:25:16 -0700

    add home, menu, about modules. Work left is to impliement tab-switching logic

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import "./styles.css";
 
 import { addHome } from "./home";
 import { addMenu } from "./menu";
+import { addAboutUs } from "./aboutUs";
 
 console.log("hello world");
 
 const contentDiv = document.querySelector("#content");
-contentDiv.appendChild(addHome());
+// contentDiv.appendChild(addHome());
 // contentDiv.appendChild(addMenu());
+contentDiv.appendChild(addAboutUs());
 
