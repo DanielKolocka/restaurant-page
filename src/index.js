bf5545a05a73becb0801c80de8e99978499ceda0
Daniel Kolocka <kolockadaniel@hotmail.com>
2025-09-05 20:41:03 -0700

    implement tab switching logic, and add nav bar styling

--- src/index.js
+++ src/index.js
@@ -4,10 +4,27 @@ import { addHome } from "./home";
 import { addMenu } from "./menu";
 import { addAboutUs } from "./aboutUs";
 
-console.log("hello world");
-
 const contentDiv = document.querySelector("#content");
-// contentDiv.appendChild(addHome());
+
+const homeButton = document.querySelector(".homeButton");
+homeButton.addEventListener("click", () => {
+    contentDiv.replaceChildren();
+    contentDiv.appendChild(addHome());
+});
+
+const menuButton = document.querySelector(".menuButton");
+menuButton.addEventListener("click", () => {
+    contentDiv.replaceChildren();
+    contentDiv.appendChild(addMenu());
+});
+
+const aboutButton = document.querySelector(".aboutButton");
+aboutButton.addEventListener("click", () => {
+    contentDiv.replaceChildren();
+    contentDiv.appendChild(addAboutUs());
+});
+
+contentDiv.appendChild(addHome());
 // contentDiv.appendChild(addMenu());
-contentDiv.appendChild(addAboutUs());
+// contentDiv.appendChild(addAboutUs());
 
